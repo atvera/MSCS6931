26f7adacd6cf6d5f06b60c167f881756190a63df
avera <atvera2@gmail.com>
2018-04-15 22:19:38 -0400

    Created DataModel. DB and entity classes were autogenerated

---
+++ RiverKeeper/RiverKeeper/AestheticsMonitoringApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AestheticsMonitoringApp.Startup))]
+namespace AestheticsMonitoringApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
