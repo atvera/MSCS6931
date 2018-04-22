26e9e8a2d85a2af7d553938be943d7e0918b5f3e
Brady Gerndt <brady.gerndt@gmail.com>
2018-04-21 20:04:53 -0500

    Webapi (#1)
    * First Commit for Web API
    
    * Create README.md
    
    * Create .gitignore

--- RiverKeeper/RiverKeeper/AestheticsMonitoringApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AestheticsMonitoringApp.MvcApplication" Language="C#" %>
---
+++ RiverKeeperAPI/RiverKeeperAPI/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using RiverKeeperDO;
+using RiverKeeperTasks;
+using System.Web.Script.Serialization;
+
+namespace RiverKeeperAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        UserTasks userTask = new UserTasks();
+        // GET: api/User
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            UserDO userDO = null;
+            try
+            {
+                userDO = userTask.GetUser();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return Json(userDO);
+        }
+
+        // GET: api/User/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/User
+        //[HttpPost]
+        //public string Post([FromBody]string value)
+        //{
+        //    Console.WriteLine(value);
+        //    return value;
+        //}
+
+         [HttpPost]
+         public bool Post(UserDO user)
+        {
+            return userTask.CreateUser(user);
+        }
+
+        // PUT: api/User/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/User/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
