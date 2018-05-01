518a1f18ca54dee11ad53ec5bd346ddee95a883d
Brady Gerndt <brady.gerndt@gmail.com>
2018-05-01 00:04:13 -0500

    Survey response apge

--- RiverKeeperAPI/Controllers/SurveyResponseController.cs
+++ RiverKeeperAPI/Controllers/SurveyResponseController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using RiverKeeperTasks;
 
 namespace RiverKeeperAPI.Controllers
 {
+
     public class SurveyResponseController : Controller
     {
         // GET: SurveyResponse
         public ActionResult Index()
         {
+            SurveyTasks surveytask = new SurveyTasks();
+            List<Dictionary<string, string>> answers = surveytask.GetSurveyResponse(2);
+
+            var jsonSerialiser = new JavaScriptSerializer();
+            var json = jsonSerialiser.Serialize(answers);
+            ViewData["Answers"] = json;
+        
             return View();
         }
     }
